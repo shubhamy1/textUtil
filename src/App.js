e8d57a8ef4092df5944924f513b5cdaef5bc56f3
Shubham Yadav <shubhamy5299@gmail.com>
2022-11-22 10:18:50 +0530

    textutil features added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode is enabled", "success");
+      document.title = "Textutils - darkMode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.title = "Textutils - lightMode";
+    }
+  };
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router> */}
+      <Navbar mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <TextForm
+          heading="Please enter the text"
+          mode={mode}
+          showAlert={showAlert}
+        />
+        {/* <About /> */}
+
+        {/* <Routes>
+          <Route exact path="/" element={<TextForm
+                heading="Please enter the text"
+                mode={mode}
+                showAlert={showAlert}
+              />}/>
+          <Route exact path="/about" element={<About />}/> */}
+        {/* <Route exact path="/recovery-password" element={<RecoveryPassword/>}/> */}
+        {/* <Route path="*" element={<NotFound/>}/> */}
+        {/* </Routes> */}
+      </div>
+      {/* </Router> */}
+    </>
   );
 }
 
